02a7c27478dc8dd6fa5c34f99171934dc0a323e1
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-12 12:42:43 -0400

    Create about page

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import RootLayout from "@/utils/layouts/RootLayout";
+import React from "react";
+
+function About() {
+  return (
+    <RootLayout>
+      <h1>About Me</h1>
+    </RootLayout>
+  );
+}
+
+export default About;
