41a92675149f0eb98e5fb2be8b399d4d5cb44903
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-12 12:20:05 -0400

    Finish styling ProjectCard

--- utils/components/portfolio/ProjectCard.tsx
+++ utils/components/portfolio/ProjectCard.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Project } from "@/utils/types/WordPressQueries";
 import Link from "next/link";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 type PortfolioCardProps = {
   project: Project;
@@ -10,7 +11,10 @@ type PortfolioCardProps = {
 
 function ProjectCard({ project, className }: PortfolioCardProps) {
   return (
-    <Link href={`/portfolio/${project.slug}`} className="group relative">
+    <Link
+      href={`/portfolio/${project.slug}`}
+      className={twMerge("aspect-w-16 aspect-h-9 group relative", className)}
+    >
       <Image
         loader={() => project.featuredMedia.src}
         src={project.featuredMedia.src}
@@ -18,10 +22,21 @@ function ProjectCard({ project, className }: PortfolioCardProps) {
         width={0}
         height={0}
         unoptimized
-        className="h-auto w-full"
+        className="object-cover"
       />
-      <div className="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-        <h2>{project.title}</h2>
+      <div
+        className={twMerge(
+          "absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center gap-2 p-4 text-center",
+          "bg-primary-900 bg-opacity-70 text-neutral-50 opacity-0 bg-blend-overlay transition-opacity duration-300 group-hover:opacity-100",
+        )}
+      >
+        <h2 className="text-xl font-semibold">{project.title}</h2>
+        {project.excerpt && (
+          <div dangerouslySetInnerHTML={{ __html: project.excerpt }} />
+        )}
+        <p className="text-sm italic">
+          {project.tools.map((tool) => tool.title).join(", ")}
+        </p>
       </div>
     </Link>
   );
