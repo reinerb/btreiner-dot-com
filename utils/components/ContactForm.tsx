aa26295cb58ee9f8d08411fd83fc447c20d43b36
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-12 15:54:34 -0400

    Add error handling to contact form

--- utils/components/ContactForm.tsx
+++ utils/components/ContactForm.tsx
@@ -3,6 +3,7 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import { twMerge } from "tailwind-merge";
 import Button from "./Button";
+import * as Yup from "yup";
 
 type ContactFormProps = {
   className?: string;
@@ -22,47 +23,96 @@ function ContactForm({ className }: ContactFormProps) {
   return (
     <Formik
       initialValues={{ name: "", email: "", message: "" }}
+      validationSchema={Yup.object({
+        name: Yup.string().required("Required"),
+        email: Yup.string().email("Invalid email address").required("Required"),
+        message: Yup.string().required("Required"),
+      })}
       onSubmit={handleSubmit}
     >
-      <Form className={twMerge("grid grid-cols-1 gap-4 sm:grid-cols-2")}>
-        <div className="flex flex-col gap-1">
-          <label htmlFor="name">Name</label>
-          <Field
-            name="name"
-            type="text"
-            className="bg-neutral-200 px-4 py-2 dark:bg-neutral-800"
-          />
-        </div>
-
-        <div className="flex flex-col gap-1">
-          <label htmlFor="email">Email</label>
-          <Field
-            name="email"
-            type="text"
-            className="bg-neutral-200 px-4 py-2 dark:bg-neutral-800"
-          />
-        </div>
-
-        <div className="flex flex-col gap-1 sm:col-span-2">
-          <div className="grid grid-cols-3">
-            <label htmlFor="message">Message</label>
-            <ErrorMessage />
+      {(formik) => (
+        <Form className={twMerge("grid grid-cols-1 gap-4 sm:grid-cols-2")}>
+          <div className="flex flex-col gap-1">
+            <div className="flex justify-between">
+              <label htmlFor="name">Name</label>
+              {formik.touched.name && formik.errors.name && (
+                <ErrorMessage
+                  name="name"
+                  render={(msg: any) => (
+                    <label
+                      htmlFor="name"
+                      className="text-red-700 dark:text-red-300"
+                    >
+                      {msg}
+                    </label>
+                  )}
+                />
+              )}
+            </div>
+            <Field
+              name="name"
+              type="text"
+              className="bg-neutral-200 px-4 py-2 dark:bg-neutral-800"
+            />
           </div>
-          <Field
-            name="message"
-            as="textarea"
-            className="bg-neutral-200 px-4 py-2 dark:bg-neutral-800"
-          />
-        </div>
 
-        <Button
-          primary
-          type="submit"
-          className="sm:col-span-2 sm:justify-self-end"
-        >
-          Submit
-        </Button>
-      </Form>
+          <div className="flex flex-col gap-1">
+            <div className="flex justify-between">
+              <label htmlFor="email">Email</label>
+              {formik.touched.email && formik.errors.email && (
+                <ErrorMessage
+                  name="email"
+                  render={(msg: any) => (
+                    <label
+                      htmlFor="email"
+                      className="text-red-700 dark:text-red-300"
+                    >
+                      {msg}
+                    </label>
+                  )}
+                />
+              )}
+            </div>
+            <Field
+              name="email"
+              type="text"
+              className="bg-neutral-200 px-4 py-2 dark:bg-neutral-800"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 sm:col-span-2">
+            <div className="flex justify-between">
+              <label htmlFor="message">Message</label>
+              {formik.touched.message && formik.errors.message && (
+                <ErrorMessage
+                  name="message"
+                  render={(msg: any) => (
+                    <label
+                      htmlFor="message"
+                      className="text-red-700 dark:text-red-300"
+                    >
+                      {msg}
+                    </label>
+                  )}
+                />
+              )}
+            </div>
+            <Field
+              name="message"
+              as="textarea"
+              className="bg-neutral-200 px-4 py-2 dark:bg-neutral-800"
+            />
+          </div>
+
+          <Button
+            primary
+            type="submit"
+            className="sm:col-span-2 sm:justify-self-end"
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
     </Formik>
   );
 }
