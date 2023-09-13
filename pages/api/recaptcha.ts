1d2cf5eaa03a7568c7f24d5885c0bbe7072ccb42
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-13 10:12:02 -0400

    Write recaptcha API handler

--- pages/api/recaptcha.ts
+++ pages/api/recaptcha.ts
@@ -1,11 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type FormSubmissionData = {
-  name: string;
-  email: string;
-  message: string;
-};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
+  if (!SECRET_KEY) {
+    return res
+      .status(500)
+      .json({ status: "failure", message: "No reCaptcha keys found" });
+  }
+
+  const { recaptchaResponse } = req.body;
+
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${SECRET_KEY}&response=${recaptchaResponse}`;
+
+  try {
+    const recaptchaResponse = await fetch(url, { method: "POST" });
+    const recaptcha = await recaptchaResponse.json();
+
+    return res.status(200).json({ status: "success", ...recaptcha });
+  } catch (e: unknown) {
+    if (typeof e === "string") {
+      return res.status(400).json({ status: "failure", message: e });
+    } else if (e instanceof Error) {
+      return res.status(400).json({ status: "failure", message: e.message });
+    }
+  }
 }
