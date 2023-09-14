394675efa52e155c9611b2f482b6b12818b235c4
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-14 17:28:10 -0400

    Adjust header styling

--- utils/components/RootLayout/Header.tsx
+++ utils/components/RootLayout/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import RootNav from "./RootNav";
 import { twMerge } from "tailwind-merge";
+import Link from "next/link";
+import Logo from "../Logo";
 
 type HeaderProps = {
   className?: string;
@@ -9,8 +11,14 @@ type HeaderProps = {
 function Header({ className }: HeaderProps) {
   return (
     <header
-      className={twMerge("flex flex-col items-center px-8 py-4", className)}
+      className={twMerge(
+        "flex flex-col items-center gap-4 px-8 py-4",
+        className,
+      )}
     >
+      <Link href="/" aria-label="Home" className="flex flex-col items-center">
+        <Logo className="w-12 fill-slate-950 transition-colors duration-200 hover:fill-slate-800 active:fill-slate-700 dark:fill-slate-50 dark:hover:fill-slate-200 dark:active:fill-slate-300 sm:w-16 lg:w-20" />
+      </Link>
       <RootNav />
     </header>
   );
--- utils/components/RootLayout/RootNav.tsx
+++ utils/components/RootLayout/RootNav.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import Link from "next/link";
-import Logo from "../Logo";
 
 function RootNav() {
   return (
     <nav>
-      <ul className="font-lg flex items-center gap-3 font-semibold sm:gap-4 sm:text-lg">
-        <li>
+      <ul className="font-lg divide-y-2 text-center font-semibold sm:text-lg">
+        <li className="py-2">
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className="py-2">
           <Link href="/portfolio">Portfolio</Link>
         </li>
-        <li>
-          <Link href="/" aria-label="Home">
-            <Logo className="w-12 fill-slate-950 transition-colors duration-200 hover:fill-slate-800 active:fill-slate-700 dark:fill-slate-50 dark:hover:fill-slate-200 dark:active:fill-slate-300 sm:w-16 lg:w-20" />
-          </Link>
-        </li>
-        <li>
+        <li className="py-2">
           <Link href="/about">About</Link>
         </li>
-        <li>
+        <li className="py-2">
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
