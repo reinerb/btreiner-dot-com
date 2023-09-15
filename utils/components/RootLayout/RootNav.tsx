7d416ede110043dd211c67ac2aa6cfb3b1261ff6
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-14 20:59:40 -0400

    Fix nav dividers

--- utils/components/RootLayout/RootNav.tsx
+++ utils/components/RootLayout/RootNav.tsx
@@ -9,7 +9,7 @@ type RootNavProps = {
 function RootNav({ activeNav, className }: RootNavProps) {
   return (
     <nav>
-      <ul className="flex flex-row gap-2 text-center font-semibold sm:gap-4 sm:text-lg md:flex-col md:gap-0 md:divide-y-2 md:text-lg">
+      <ul className="flex flex-row gap-2 text-center font-semibold sm:gap-4 sm:text-lg md:flex-col md:gap-0 md:divide-y-2 md:divide-slate-800 md:text-lg md:dark:divide-slate-200">
         <li className="md:py-2">
           <Link className={activeNav === "home" ? "active" : ""} href="/">
             Home
