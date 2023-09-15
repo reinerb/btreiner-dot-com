6dc019ee4b263c7a2b49343b0e7eedcc6d36c38e
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-09-14 20:57:49 -0400

    Set up homepage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,23 +41,33 @@ export default function Home({ projects }: HomepageProps) {
       title="Ben Reiner | Web Developer"
       metaDescription="Hi! I'm Ben Reiner, a freelance frontend developer. This is a collection of some cool projects I've made."
       activeNav="home"
-      className="flex flex-col gap-4"
+      className="grid place-items-center"
     >
-      <p>Hi! I'm</p>
-      <h1 className="text-9xl font-bold text-primary-800 dark:text-primary-300">
-        Ben{" "}
-        <Logo className="inline w-36 fill-primary-800 dark:fill-primary-300" />
-        einer
-      </h1>
-      <ul className="flex gap-4 divide-x-2 divide-neutral-700 text-lg text-neutral-800 dark:divide-neutral-300 dark:text-neutral-200">
-        <li>Frontend</li>
-        <li className="pl-4">React</li>
-        <li className="pl-4">WordPress</li>
-      </ul>
-      <p>Let's build something incredible together.</p>
-      <LinkButton href="/contact" className="max-w-fit">
-        Contact Me
-      </LinkButton>
+      <div className="container flex flex-col items-center gap-8">
+        <p className="font-bold sm:text-lg md:text-xl">Hi! I'm</p>
+        <h1 className="text-5xl font-bold text-primary-800 dark:text-primary-300 sm:text-7xl md:text-8xl xl:text-9xl">
+          Ben{" "}
+          <Logo className="inline w-16 fill-primary-800 dark:fill-primary-300 sm:w-24 md:w-28 xl:w-32" />
+          einer
+        </h1>
+        <ul className="flex gap-4 divide-x-2 divide-slate-500 text-slate-700 dark:text-slate-300 sm:text-lg">
+          <li>Frontend</li>
+          <li className="pl-4">React</li>
+          <li className="pl-4">WordPress</li>
+        </ul>
+        <div className="flex max-w-fit flex-col items-center gap-4 bg-primary-800 p-4 text-center text-slate-50 dark:bg-primary-300 dark:text-slate-950">
+          <p className="font-heading font-semibold sm:text-lg">
+            Let's build something incredible together.
+          </p>
+          <LinkButton
+            href="/contact"
+            className="max-w-fit border-2 font-semibold"
+            inverted
+          >
+            Contact Me
+          </LinkButton>
+        </div>
+      </div>
     </RootLayout>
   );
 }
