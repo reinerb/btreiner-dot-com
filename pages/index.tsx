d928bc96cddca6a5e5f1f31e19baaf8d4b92816e
Ben Reiner <benjamin.t.reiner@gmail.com>
2023-10-02 12:29:37 -0400

    Adjust homepage content

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,28 +29,23 @@ export default function Home() {
       metaDescription="Hi! I'm Ben Reiner, a freelance frontend developer. This is a collection of some cool projects I've made."
       activeNav="home"
       noSidebar
-      className="grid h-screen place-items-center bg-gradient-to-b from-slate-400 via-slate-50 to-slate-400 dark:from-slate-950 dark:via-slate-800 dark:to-slate-950"
+      className="grid h-screen place-items-center bg-gradient-to-b from-slate-50 via-slate-200 to-slate-50 dark:from-slate-950 dark:via-slate-800 dark:to-slate-950"
     >
       <HamburgerNav activeNav="home" className="fixed right-5 top-5" />
       <div className="container flex flex-col items-center gap-8">
         <p
-          className={`font-bold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl ${anybody.className}`}
+          className={`font-extrabold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl ${anybody.className}`}
         >
           Hi! I'm
         </p>
         <h1
-          className={`text-4xl font-bold text-primary-800 dark:text-primary-300 sm:text-7xl lg:text-8xl xl:text-9xl ${anybody.className}`}
+          className={`text-4xl font-black text-primary-800 dark:text-primary-300 sm:text-7xl lg:text-8xl xl:text-9xl ${anybody.className}`}
           aria-label="Ben Reiner"
         >
           Ben{" "}
           <Logo className="inline w-16 fill-primary-800 dark:fill-primary-300 sm:w-24 md:w-28 xl:w-32" />
           einer
         </h1>
-        {/* <ul className="flex divide-x-2 divide-slate-500 text-slate-700 dark:text-slate-300 sm:text-lg">
-          <li className="px-4">Frontend</li>
-          <li className="px-4">React</li>
-          <li className="px-4">WordPress</li>
-        </ul> */}
         <ScrollingText elements={skills} />
         <div className="flex flex-wrap justify-center gap-4">
           <LinkButton small href="/About">
@@ -76,15 +71,11 @@ export default function Home() {
             LinkedIn
           </LinkButton>
         </div>
-        <div className="flex max-w-fit flex-col items-center gap-4 bg-primary-800 p-4 text-center text-slate-50 dark:bg-primary-300 dark:text-slate-950">
+        <div className="flex max-w-fit flex-col items-center gap-4 bg-slate-300 p-4 text-center dark:bg-slate-700">
           <p className="font-heading font-semibold sm:text-lg">
-            Ready to make something incredible?
+            Want to make something incredible?
           </p>
-          <LinkButton
-            href="/contact"
-            className="max-w-fit border-2 font-semibold"
-            inverted
-          >
+          <LinkButton href="/contact" className="max-w-fit font-semibold">
             Contact Me
           </LinkButton>
         </div>
